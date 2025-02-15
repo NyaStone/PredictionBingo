89fcd373543324c0acdd5ac3301349a8b058d385
Pierre Lenertz <pierre@lenertz.fr>
2025-02-15 11:51:12 +0100

    Added credits footer

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -4,16 +4,22 @@ import { Settings } from "./Settings";
 
 function MainSection() {
     return (
-        <div className="w-full px-4 mx-auto md:max-w-2xl lg:max-w-3xl xl:max-w-4xl min-h-screen bg-gray-900 py-8 ">
-            <Settings/>
-            <div className="flex flex-col">
-                <div className="order-2 md:order-1">
-                    <ItemList/>
-                </div>
-                <div className="order-1 md:order-2">
-                    <Grid/>
+        <div className="w-full px-4 mx-auto md:max-w-2xl lg:max-w-3xl xl:max-w-4xl min-h-screen bg-gray-900 py-8 
+                        flex flex-col">
+            <div className="flex-1">
+                <Settings/>
+                <div className="flex flex-col">
+                    <div className="order-2 md:order-1">
+                        <ItemList/>
+                    </div>
+                    <div className="order-1 md:order-2">
+                        <Grid/>
+                    </div>
                 </div>
             </div>
+            <footer className="text-center text-gray-400 text-sm py-4">
+                Created by Pierre Lenertz
+            </footer>
         </div>
     );
 }
